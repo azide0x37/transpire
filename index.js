dc200e2a4f92e9b85dbcdf26b0970c2f72f4acbb
Alexander Templeton <blakmatr@gmail.com>
2019-03-26 21:23:38 -0500

    massively shorter glitch filter

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ const dryer = new Gpio(17, {
 });
 
 // Level must be stable for 65s before an alert event is emitted.
-washer.glitchFilter(650000);
-dryer.glitchFilter(650000);
+washer.glitchFilter(10000);
+dryer.glitchFilter(10000);
 
 washer.on('alert', async (level, tick) => sendApplianceNotification(await storage.getItem('chatIds'), level === 1, 'Washer'))
 dryer.on('alert', async (level, tick) => sendApplianceNotification(await storage.getItem('chatIds'), level === 1, 'Dryer'))
